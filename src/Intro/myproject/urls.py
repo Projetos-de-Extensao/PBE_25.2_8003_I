ed44e3de35bbc149807c30f7540eb3910ff61d49
Roberto Ramos <202408320931@alunos.ibmec.edu.br>
2025-10-08 11:42:49 -0300

    update django

---
+++ src/Intro/myproject/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+   #  path('', include('myapp.urls')),  # URLs tradicionais
+    path('api/', include('myapp.api_urls')),  # URLs da API
+]
